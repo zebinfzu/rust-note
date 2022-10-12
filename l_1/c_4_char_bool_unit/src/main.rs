f2af6a1bb788748739c9bb2d930745d4c50d1f67
zhangzebin <1185261506@qq.com>
2022-10-12 17:14:24 +0800

    1: statement expression

--- l_1/c_4_char_bool_unit/src/main.rs
+++ l_1/c_4_char_bool_unit/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     println!("字符'中'占用了{}字节的内存大小", std::mem::size_of_val(&x));
     // 2. boolean 1Byte
     let t: bool = true;
-    // 3. 单元类型()，唯一值只有()，main函数返回值就是()，rust中没有返回值的函数被称为发散函数(diverge function)，占内存0
+    // 3. 单元类型unit()，唯一值只有()，main函数返回值就是()，rust中没有返回值的函数被称为发散函数(diverge function)，占内存0
 }
