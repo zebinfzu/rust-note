8aa507548c2888ab771d64026b88a5acae378d64
zhangzebin <1185261506@qq.com>
2022-11-28 14:43:00 +0800

    the: 13 minigrep 16 message send

---
+++ The_Book/13/minigrep/src/main.rs
@@ -0,0 +1,13 @@
+use std::{env, process};
+use minigrep::{Config, run};
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
